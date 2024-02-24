21c7232c6dba928df4914f7a80b6e76057f75c7c
Kaylars <satitiputri.kayla@gmail.com>
2024-02-24 10:12:50 +0700

    percobaan 2

--- SMT 2/PERTEMUAN 02/Buku15.java
+++ SMT 2/PERTEMUAN 02/Buku15.java
@@ -1,6 +1,18 @@
-public class Buku15 {
+public class buku15 {
     String judul, pengarang;
     int halaman, stok, harga;
+
+    public buku15() {
+
+    }
+
+    public buku15(String j, String p, int hal, int s, int h) {
+        judul = j;
+        pengarang = p;
+        halaman = hal;
+        stok = s;
+        harga = h;
+    }
 
     void tampilInformasi() {
         System.out.println("Judul          : " + judul);
