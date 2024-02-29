e22493f8b7113fcd4e8108f2670c5601a3d558c5
Kaylars <satitiputri.kayla@gmail.com>
2024-02-29 22:40:31 +0700

    add constructor parameter + instansiasi

--- SMT 2/PERTEMUAN 03/persegiPanjangDemo15.java
+++ SMT 2/PERTEMUAN 03/persegiPanjangDemo15.java
@@ -5,15 +5,18 @@ public class persegiPanjangDemo15 {
         Scanner sc = new Scanner(System.in);
 
         persegiPanjang15[] arrayOfPersegiPanjang = new persegiPanjang15[3];
+        int panjang, lebar;
         
         // menerima input
         for (int i = 0; i < 3; i++) {
             arrayOfPersegiPanjang[i] = new persegiPanjang15();
             System.out.println("\nPersegi panjang ke-" + i);
             System.out.print("Masukkan panjang : ");
-            arrayOfPersegiPanjang[i].panjang = sc.nextInt();
+            panjang = sc.nextInt();
             System.out.print("Masukkan lebar   : ");
-            arrayOfPersegiPanjang[i].lebar = sc.nextInt();
+            lebar = sc.nextInt();
+
+            arrayOfPersegiPanjang[i] = new persegiPanjang15(panjang, lebar);
         }
 
         persegiPanjang15 persegipanjang = new persegiPanjang15();
