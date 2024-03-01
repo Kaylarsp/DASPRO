7874f88890eeb4e1559ffb0d5dd190df7d39c870
Kaylars <satitiputri.kayla@gmail.com>
2024-03-01 20:03:13 +0700

    add tugas 2 & laporan praktikum

--- SMT 2/PERTEMUAN 03/atm.java
+++ SMT 2/PERTEMUAN 03/atm.java
@@ -1,81 +1,97 @@
-import java.util.Scanner;;
+import java.util.Scanner;
 
 public class atm {
-    public int pin;
-    public String rekening;
-    public int saldo = 100000;
-    
+    private int pin;
+    private String rekening;
+    private int saldo = 100000;
+
     public atm() {}
-    public atm(int p, String rek, int s) {
+
+    public atm(int p, String rek) {
         pin = p;
         rekening = rek;
-        saldo = s;
-    }
-    
-    public static void login(int pin) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("\nMasukkan pin : ");
-        pin = sc.nextInt();
-        tampilkanMenu();
     }
 
-    public static void tampilkanMenu() {
+    public void login() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nMasukkan pin : ");
+        int inputPin = sc.nextInt();
+        if (inputPin == pin) {
+            tampilkanMenu();
+        } else {
+            System.out.println("Pin salah, silakan coba lagi.");
+            login();
+        }
+    }
+
+    public void tampilkanMenu() {
         Scanner sc = new Scanner(System.in);
         System.out.println();
-        System.out.print("Pilih menu : ");
-        int menu = sc.nextInt();
+        System.out.println("-------------------");
+        System.out.println("\tMENU\t");
+        System.out.println("-------------------");
         System.out.println("1. CEK SALDO");
         System.out.println("2. TARIK TUNAI");
         System.out.println("3. SETOR TUNAI");
         System.out.println("4. TRANSFER SALDO");
+        System.out.println("-------------------");
+        System.out.print("\nPilih menu : ");
+        int menu = sc.nextInt();
         switch (menu) {
             case 1:
-                cekSaldo(menu);
+                cekSaldo();
                 break;
             case 2:
-                tarikTunai(menu, menu);;
+                tarikTunai();
                 break;
             case 3:
-                setorTunai(menu, menu);;
+                setorTunai();
                 break;
             case 4:
-                transferSaldo(menu, menu);;
+                transferSaldo();
                 break;
             default:
                 break;
         }
     }
 
-    public static void cekSaldo(int saldo) {
-        System.out.print("Saldo anda : " + saldo);
+    public void cekSaldo() {
+        System.out.println();
+        System.out.println("Saldo anda : " + saldo);
     }
 
-    public static void tarikTunai(int jumlah, int saldo) {
+    public void tarikTunai() {
+        System.out.println();
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan nominal tarik tunai : ");
-        jumlah = sc.nextInt();
+        int jumlah = sc.nextInt();
         if (jumlah <= saldo) {
             saldo -= jumlah;
+            System.out.println("Transaksi berhasil.");
         } else {
-            System.out.println("Saldo anda tidak cukup");
+            System.out.println("Saldo anda tidak cukup.");
         }
     }
 
-    public static void setorTunai(int setor, int saldo) {
+    public void setorTunai() {
+        System.out.println();
         Scanner sc = new Scanner(System.in);
-        System.out.print("Masukkan nominal setor tunai");
-        setor = sc.nextInt();
-        saldo += setor;
+        System.out.print("Masukkan nominal setor tunai : ");
+        int jumlah = sc.nextInt();
+        saldo += jumlah;
+        System.out.println("Transaksi berhasil.");
     }
 
-    public static void transferSaldo(int tf, int saldo) {
+    public void transferSaldo() {
+        System.out.println();
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan nominal trasnfer : ");
-        tf = sc.nextInt();
-        if (tf <= saldo) {
-            saldo -= tf;
+        int jumlah = sc.nextInt();
+        if (jumlah <= saldo) {
+            saldo -= jumlah;
+            System.out.println("Transaksi berhasil.");
         } else {
-            System.out.println("Saldo anda tidak cukup");
+            System.out.println("Saldo anda tidak cukup.");
         }
     }
 }
--- SMT 2/PERTEMUAN 03/tugas2_15.java
+++ SMT 2/PERTEMUAN 03/tugas2_15.java
@@ -1,5 +1,6 @@
 public class tugas2_15 {
     public static void main(String[] args) {
-        
+        atm transaksi = new atm(1234, "1234567890");
+        transaksi.login();
     }
 }
