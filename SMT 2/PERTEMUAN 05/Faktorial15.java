7249137878b8d63c913bec23e19e6706c092403f
Kaylars <satitiputri.kayla@gmail.com>
2024-03-13 09:59:55 +0700

    add percobaan 2

--- SMT 2/PERTEMUAN 05/Faktorial15.java
+++ SMT 2/PERTEMUAN 05/Faktorial15.java
@@ -1,7 +1,7 @@
 public class Faktorial15 {
     public int nilai;
     public Faktorial15() {}
-    // menghitung faktorial dari n dengan cara mengalikan n dengan semua bilangan positif dari 1 hingga n
+    // menghitung faktorial dari n dengan cara mengalikan n dg semua bilangan positif dr 1 smp n
     public int faktorialBF(int n) {
         int fakto = 1; // dideklarasikan dengan nilai 1 krn faktorial dr 0 adlh 1, dan perkalian akan dimulai dr angka tsb
         for (int i = 1; i <= n; i++) {
