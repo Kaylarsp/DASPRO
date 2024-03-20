6e9766dfe51c712e805ac14af23567250bb4f675
Kaylars <satitiputri.kayla@gmail.com>
2024-03-20 12:08:20 +0700

    add laporan

--- SMT 2/PERTEMUAN 06/HotelService15.java
+++ SMT 2/PERTEMUAN 06/HotelService15.java
@@ -1,8 +1,20 @@
 public class HotelService15 {
-    Hotel15 rooms = new Hotel15(null, null, 0, null);
+    Hotel15 rooms[] = new Hotel15[5];
+    int idx;
 
-    void tambah(Hotel15 h) {}
-    void tampilAll() {}
+    void tambah(Hotel15 h) {
+        if (idx < rooms.length) {
+            rooms[idx] = h;
+        } else {
+            System.out.println("Data sudah penuh!!");
+        }
+    }
+
+    void tampilAll() {
+        System.out.println("Nama : ");
+    }
     void bubbleSort() {}
     void selectionSort() {}
+
+
 }
